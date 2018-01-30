023e6ee3d542d2f3858d74c412e4dc5e3509f3eb
unknown <guoni0909@gmail.com>
2018-01-30 15:19:29 -0800

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,16 @@
 
+$("#playAgain").hide();
 //after the player clicks Start button that will trigger the startGame function
 $("#start").on("click",startGame);
 //after the player picks answers will trigger the handleClick function
 $("#options").on("click", handleClick);
+
 
 var counterMax = 6;
 var counter = counterMax;
 var timer;
 var wins = 0;
 var losses = 0;
-var questionNum = 0;
 var optionsDiv = {
 	options1: $("#options1"),
 	options2: $("#options2"),
@@ -18,10 +19,12 @@ var optionsDiv = {
 }
 var questionDiv = $("#questions");
 var currentQuestion = 0;
-var maxIndex = 3;
+var questionIndex = 3;
 var active = false;
+//using an array to save players answer
 var playerPick = [];
-//var images = ["images/trump.jpg", "images/george.jpg"];
+var images = ["images/Obama.gif", "images/Reagan.gif","images/Trump.gif","images/Washington.gif"];
+var j = 0;
 
 //questions that will display each 30 seconds
 var items = [
@@ -37,38 +40,40 @@ var items = [
 		options: ["George Washington","Thomas Jefferson","Donald Trump","Michael Jackson"]
 	},
 	{
-		question: "3. Which persident of United States has/had a red hair?", 
-		answer: "Donald Trump",
-		options: ["George Washington","Thomas Jefferson","Donald Trump","Michael Jackson"]
+		question: "3. Which persident of United States loved Jelly beans?", 
+		answer: "Ronald Reagan",
+		options: ["Ronald Reagan","Thomas Jefferson","Donald Trump","Michael Jackson"]
 	},
 	{
-		question: "4. Which persident of United States has/had a red hair?", 
-		answer: "Donald Trump",
-		options: ["George Washington","Thomas Jefferson","Donald Trump","Michael Jackson"]
+		question: "4. Who was the last persident of United States?", 
+		answer: "Barack Obama",
+		options: ["George W. Bush","Bill Clinton","Barack Obama","George H.w. Bush"]
 	}
 ]
 
 function startGame() {
-	
-	$("#start").hide();
+
+	$("#totalPoints").hide();
+
 	//timer counts down every 1 second
 	timer = setInterval(gameTimer, 1000);
 	counter = counterMax;
 	active = true;
+	$("#counter").text("Time Remaining: 30 Seconds!");
 	displayQuestion(items[currentQuestion]);
 }
 
 function handleClick(event) {
-	
 	if (active) {
 		var userChoice = event.target.innerHTML
 		console.log(userChoice)
 		//if the user picks the right answer
 		// wins will get one point
-		if (userChoice === items[currentQuestion].answer) {
+		if (userChoice === items[currentQuestion].answer && userChoice === images[j]) {
 			console.log('Correct!')
 			playerPick.push(true);
 			wins++;
+			showImages();
 			$("#totalWins").text("Wins: " + wins);
 			console.log("this is player picks: " + playerPick);
 		} 
@@ -86,11 +91,12 @@ function handleClick(event) {
 }
 
 function changeQuestion() {
-	if (currentQuestion === maxIndex) {
+	if (currentQuestion === questionIndex) {
 		//hand the end of game
 		endOfGame();
 
-	} else {
+	} 
+	else {
 		counter = counterMax;
 		currentQuestion++;
 		displayQuestion(items[currentQuestion]);
@@ -100,8 +106,11 @@ function changeQuestion() {
 function endOfGame() {
 	console.log(playerPick);
 	active = false;
+	$("#options").hide();
+	$("#display").hide();
+	$("#totalPoints").show();
+	$("#playAgain").show();
 }
-
 
 function gameTimer() {
 	if (active) {
@@ -127,5 +136,7 @@ function displayQuestion(question) {
 } 
 
 function showImages() {
+
+	$("#imageHolder").html("<img src=' " + images[j] + " ' width='200px'/>");
 	
 }
